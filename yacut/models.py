633b7455efee53cbc1fc4f24a6e9f3dfd711a531
Natolin Artem <arnat64@yandex.ru>
2023-09-11 11:33:54 +0300

    refac

--- yacut/models.py
+++ yacut/models.py
@@ -2,11 +2,13 @@ from datetime import datetime
 
 from yacut import db
 
+from .utils import Constants
+
 
 class URLMap(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     original = db.Column(db.Text, nullable=False, unique=True)
-    short = db.Column(db.String(16))
+    short = db.Column(db.String(Constants.MAX_URL_LENGHT))
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
 
     @property
